41fdb994e9fca72c0a4496fa22deeb90a0c0dce7
Steven Brendtro <steven.brendtro@gmail.com>
2019-10-25 13:32:59 -0500

    Initial commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Steven Brendtro
+ * @copyright Copyright (c) 2019 Steven Brendtro and IDP (https://goidp.com)
+ * @package IDP_CustomApiField
+ */
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'IDP_CustomApiField',
+    __DIR__
+);
